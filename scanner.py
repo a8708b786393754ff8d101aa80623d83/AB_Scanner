e266e938ddbb6814e022f60a216fbed9717e7ee0
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-03-20 23:33:22 +0100

    setting code(object, function and import)

--- scanner.py
+++ scanner.py
@@ -1,11 +1,11 @@
-#!/usr/local/bin/python3.10
+#!/usr/bin/python3.10
 import sys 
 import time 
 import ipaddress
 import logging
 
 from  scanner_host import scan_arp, scan_icmp
-from  scanner_protocol import get_protocole_name, ScanTCP, ScanUDP
+from  scanner_protocol import scan_syn, scan_udp
 
 logging.getLogger('scapy.runtime').setLevel(logging.ERROR)
 
@@ -14,15 +14,8 @@ if len(sys.argv) == 1:
                 'ex: ./scanner.py 192.168.0.10/24 OU'
                 './scanner.py ICMP 192.168.0.10')
 
-TARGET = sys.argv[-1]
+TARGET = sys.argv[1]
 start = time.time()
-
-# recuperer les port tcp, udp est leur nom
-tcp_ports, udp_ports, names_port = get_protocole_name()
-
-        
-tcp_scan = ScanTCP(tcp_port=tcp_ports)
-udp_scan = ScanUDP(udp_port=udp_ports)
 
 if len(sys.argv) == 2:
     if '/' in TARGET: 
@@ -32,15 +25,14 @@ if len(sys.argv) == 2:
             scan_icmp(ip)
             scan_arp(ip)
 
-            tcp_scan.SYN(ip)
-            udp_scan.run(ip)
-            print()
+            scan_syn(ip, False)
+            scan_udp(ip, False)
     else: 
         scan_icmp(TARGET)
         scan_arp(TARGET)
 
-        tcp_scan.SYN(TARGET)
-        udp_scan.run(TARGET)
+        scan_syn(TARGET, False)
+        scan_udp(TARGET, False)
 
 elif len(sys.argv) > 2: 
     for arg in sys.argv[1::]:
@@ -51,15 +43,15 @@ elif len(sys.argv) > 2:
                 match arg:        
                     case 'icmp' : scan_icmp(ip)
                     case 'arp' :  scan_arp(ip)
-                    case 'syn' :  tcp_scan.SYN(ip)
-                    case 'udp' :  udp_scan.run(ip)
+                    case 'syn' :  scan_syn(ip, False)
+                    case 'udp' :  scan_udp(ip, False)
     
         else: 
             match arg: 
                 case 'icmp' : scan_icmp(TARGET) 
                 case 'arp' :  scan_arp(TARGET)
-                case 'syn' :  tcp_scan.SYN(TARGET)
-                case 'udp' :  udp_scan.run(TARGET)
+                case 'syn' :  scan_syn(TARGET, False)
+                case 'udp' :  scan_udp(TARGET, False)
 
 
 print(time.time() - start)
