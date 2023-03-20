5f1db6e991891efa63af6a301535b780f53ab25b
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-03-20 23:32:37 +0100

    formatting code and setting import

--- scanner_host.py
+++ scanner_host.py
@@ -1,20 +1,23 @@
-#!/usr/local/bin/python3.10
-from scapy.all import Ether, IP, srp, ARP, ICMP
+from scapy.sendrecv import srp
+from scapy.layers.inet import IP, ICMP
+from scapy.layers.l2 import ARP, Ether
+
 
 def scan_icmp(target: str):
     pkt = Ether() / IP(dst=target) / ICMP()
-    ans, _ = srp(pkt, timeout=1, verbose=0) 
+    ans, _ = srp(pkt, timeout=1, verbose=0)
     # envoie du packet avec la methode srp pour avoir l'adresses mac
-    
-    for element in ans:
-        if element[1]['ICMP'].type == 0: 
-            mac_target = element[1]['Ethernet'].src
-            print(target,'--->',mac_target,' ::ICMP')
 
-def scan_arp(target: str): 
+    for element in ans:
+        if element[1][ICMP].type == 0:
+            mac_target = element[1][Ether].src
+            print(target, '--->', mac_target, ' ::ICMP')
+
+
+def scan_arp(target: str):
     pkt = Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(pdst=target)
     ans, _ = srp(pkt, verbose=0, timeout=1)
 
     for element in ans:
-        mac_target = element[1]["Ethernet"].src
-        print(target,'--->',mac_target,' ::ARP')
+        mac_target = element[1][Ether].src
+        print(target, '--->', mac_target, ' ::ARP')
