7ede26c162c6ab4d954c1c8d4372e3a13febbec6
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-03-21 21:18:25 +0100

    add dosctring

--- scanner.py
+++ scanner.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3.10
-import sys 
-import time 
+import sys
+import time
 import ipaddress
 import logging
 
-from  scanner_host import scan_arp, scan_icmp
-from  scanner_protocol import scan_syn, scan_udp, set_protocole_name
+from scanner_host import scan_arp, scan_icmp
+from scanner_protocol import scan_syn, scan_udp, set_protocole_name
 
 logging.getLogger('scapy.runtime').setLevel(logging.ERROR)
 
-if len(sys.argv) == 1: 
-        exit('Entrer la cible avec le type de scan'
-                'ex: ./scanner.py 192.168.0.10/24 OU'
-                './scanner.py ICMP 192.168.0.10')
+if len(sys.argv) == 1:
+    exit('Entrer la cible avec le type de scan'
+         'ex: ./scanner.py 192.168.0.10/24 OU'
+         './scanner.py ICMP 192.168.0.10')
 
 TARGET = sys.argv[1]
 start = time.time()
-set_protocole_name()
+
+set_protocole_name()  # NOTE met en place les numero des port tcp udp
 
 if len(sys.argv) == 2:
-    if '/' in TARGET: 
-        for ip in  ipaddress.ip_network(TARGET, False).hosts(): 
+    if '/' in TARGET:
+        for ip in ipaddress.ip_network(TARGET, False).hosts():
             ip = str(ip)
 
             scan_icmp(ip)
@@ -28,33 +29,31 @@ if len(sys.argv) == 2:
 
             scan_syn(ip, False)
             scan_udp(ip, False)
-    else: 
+    else:
         scan_icmp(TARGET)
         scan_arp(TARGET)
 
         scan_syn(TARGET, False)
         scan_udp(TARGET, False)
 
-elif len(sys.argv) > 2: 
+elif len(sys.argv) > 2:
     for arg in sys.argv[1::]:
         arg = arg.lower()
         if '/' in TARGET:
-            for ip in  ipaddress.ip_network(TARGET, False).hosts(): 
+            for ip in ipaddress.ip_network(TARGET, False).hosts():
                 ip = str(ip)
-                match arg:        
-                    case 'icmp' : scan_icmp(ip)
-                    case 'arp' :  scan_arp(ip)
-                    case 'syn' :  scan_syn(ip, False)
-                    case 'udp' :  scan_udp(ip, False)
-    
-        else: 
-            match arg: 
-                case 'icmp' : scan_icmp(TARGET) 
-                case 'arp' :  scan_arp(TARGET)
-                case 'syn' :  scan_syn(TARGET, False)
-                case 'udp' :  scan_udp(TARGET, False)
+                match arg:
+                    case 'icmp': scan_icmp(ip)
+                    case 'arp':  scan_arp(ip)
+                    case 'syn':  scan_syn(ip, False)
+                    case 'udp':  scan_udp(ip, False)
+
+        else:
+            match arg:
+                case 'icmp': scan_icmp(TARGET)
+                case 'arp':  scan_arp(TARGET)
+                case 'syn':  scan_syn(TARGET, False)
+                case 'udp':  scan_udp(TARGET, False)
 
 
 print(time.time() - start)
-
-
--- scanner_protocol.py
+++ scanner_protocol.py
@@ -9,7 +9,9 @@ ports_udp = []
 name_port = []
 
 
-def set_protocole_name():
+def set_protocole_name()-> None:
+    """Recupere est ajoute dans des liste recpective, leur nom est le port TCP ou UDP."""
+
     with open('port_protocol.json', 'r') as j_port:
         data = json.load(j_port)
 
@@ -26,7 +28,15 @@ def set_protocole_name():
             name_port.append(element[1]['name'])
 
 
-def state(ans, port: int):
+def state(ans, port: int)-> None:
+    """Regarde le status des paquets de reponse, elle peut determinée les status 
+    de paquet de TCP/SYN, ICMP noel est UDP
+
+    Args:
+        ans (_type_): liste de paquet de reponse 
+        port (int): port
+    """
+
     for element in ans:
         target = element[1][IP].src
 
@@ -48,7 +58,14 @@ def state(ans, port: int):
             print(target, '--->', port_udp_open, '::UDP')
 
 
-def get_headers(target: str, port: int):
+def get_headers(target: str, port: int) -> None:
+    """Effectue une requete au serveur web si le port est connue comme un port de serveur web
+
+    Args:
+        target (str): cible ip
+        port (int): port
+    """
+
     try:
         if port == 443:
             resp = requests.get(f'https://{target}/')
@@ -68,28 +85,43 @@ def get_headers(target: str, port: int):
     print('Il n ya pas de service web executer sur ce port')
 
 
-def scan_syn(target: str, all_port: bool = True):
+def scan_syn(target: str, all_port: bool = True) -> None:
+    """Forge un paquet TCP avec le flag SYN dessus
+
+    Args:
+        target (str): cible ip
+        all_port (bool, optional): tester les port reservée. pour tester tout les port.
+
+    """
+
     pkt = IP(dst=target) / TCP(flags='S', sport=1234)
 
     for port in ports_tcp:
         port = int(port)
 
-        if  not all_port and port > 1023:
-            return None 
-            
+        if not all_port and port > 1023:  # NOTE port reserver ou pas ?
+            return None
+
         pkt[TCP].dport = port
         ans, _ = sr(pkt, verbose=0, timeout=2)
         state(ans, port)
 
 
-def scan_udp(target: str, all_port: bool = True):
+def scan_udp(target: str, all_port: bool = True) -> None:
+    """Forge un paquet udp pour le scan upd noel
+
+    Args:
+        target (str): ip cible 
+        all_port (bool, optional): tester les port reservée. pour tester tout les port.
+    """
+    
     pkt = IP(dst=target) / UDP(sport=1234)
 
     for port in ports_udp:
         port = int(port)
 
-        if not all_port and port > 1023:
-            return None 
+        if not all_port and port > 1023:  # NOTE port reserver ou pas ?
+            return None
 
         pkt[UDP].dport = port
         ans, _ = sr(pkt, verbose=0, timeout=2)
